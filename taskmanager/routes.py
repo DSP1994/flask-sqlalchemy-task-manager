93a8c8bcc5c7e26ad570d297d5c841b96dc1374c
David <parrydavid@live.co.uk>
2022-07-27 15:01:33 +0000

    added an if request.method for when someone clicks on the 'add category' button that was just made

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 # line 3 is importing from taskmanager folder, models.py,
@@ -18,4 +18,9 @@ def categories():
 @app.route("/add_category", methods=["GET", "POST"])
 # get and post , needed because it's submitting a form
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
