3e37ebb70c2418912e1d1d569d389ef8c17d3366
David <parrydavid@live.co.uk>
2022-07-27 12:10:26 +0000

    set up the class task and category, connected it to the routes.py file and then set up the 'create database taskmanger' as well as using python3 to migrate the models(classes) to the new database.

---
+++ taskmanager/models.py
@@ -0,0 +1,39 @@
+from taskmanager import db
+
+
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete",
+                            lazy=True)
+    # nullable means it's a required field
+    # db.relationship isnt visible on main site
+    # it is just a reference
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+
+
+class Task(db.Model):
+    # schema for the Task model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    # nullable means it's a required field
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id",
+                            ondelete="CASCADE"), nullable=False)
+    # category.id has been pulled from the 'Category' class above 'id = ...'
+    # CASCADE is needed as each task needs a category, you can have many
+    # tasks per category but not many category's per task.
+    # If a category is deltd it will also delete the tasks assosiated with it.
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Category, Task
+# line 3 is importing from taskmanager folder, models.py,
+# and is finding the 'Category and Task classes made in there
 
 
 @app.route("/")
